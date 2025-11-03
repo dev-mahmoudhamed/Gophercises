d3589ec959663fbab274a125a3566353bce219d7
dev.mahmoudhamed@gmail.com <dev.mahmoudhamed@gmail.com>
2025-11-03 10:35:39 +0200

    Add time limit feature and improve answer handling in quiz game

---
+++ README.md
@@ -0,0 +1,29 @@
+# Gophercises Solutions by Mahmoud Hamed 🇵🇸
+
+This repository contains my solutions for the **[Gophercises](https://courses.calhoun.io/courses/cor_gophercises)** course by Jon Calhoun.  
+I'm using this project to practice Go by solving real, hands-on exercises that cover topics like file I/O, interfaces, concurrency, and web development.
+
+---
+
+## 📘 About the Course
+
+**Gophercises** is a series of coding exercises designed to help Go developers strengthen their skills by building small, practical projects.  
+Each exercise focuses on a particular concept — from reading CSV files and building CLI tools, to working with web servers and databases.
+
+---
+
+## 🏃‍♂️ How to Run
+
+To test or run any exercise:
+
+```bash
+# Clone the repo
+git clone https://github.com/dev-mahmoudhamed/gophercises.git
+cd gophercises
+
+# Go into an exercise folder (replace <exercise-name> with the folder name)
+cd <exercise-name>
+
+# Run the code
+go run .
+```
--- quiz_game/quiz.go
+++ quiz_game/quiz.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
 	filePath := "problems.csv"
 	totalQuestions := 0
 	rightAnswers := 0
+	limit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
 
 	f, err := os.Open(filePath)
 	if err != nil {
@@ -25,21 +29,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// totalQuestions = len(records)
+	totalQuestions = len(records)
 
 	for i, record := range records {
 		question := record[0]
 		answer := record[1]
-		totalQuestions++
 
 		fmt.Printf("Problem #%d: %s = ", i, question)
 
-		var answerInput string
-		fmt.Scan(&answerInput)
+		answerCh := make(chan string)
+		go readUserAnswer(answerCh)
 
-		if answerInput == answer {
-			rightAnswers++
+		timeout := time.After(time.Second * time.Duration(*limit))
+
+		select {
+		case answerInput := <-answerCh:
+			if answerInput == answer {
+				rightAnswers++
+			}
+		case <-timeout:
+			fmt.Printf("\nYou scored %d out of %d.\n", rightAnswers, totalQuestions)
+			return
 		}
+
 	}
-	fmt.Printf("You scored %d out of %d.\n", rightAnswers, totalQuestions)
+	fmt.Printf("\nYou scored %d out of %d.\n", rightAnswers, totalQuestions)
+}
+
+func readUserAnswer(ch chan<- string) {
+	var answerInput string
+	fmt.Scan(&answerInput)
+	ch <- answerInput
 }
